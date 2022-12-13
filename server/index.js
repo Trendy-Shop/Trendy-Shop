4255ce706d22339dfbc8649422d10d7dbcf7a9ec
ahmed henchiri <ahmedhenchiri00@gmail.com>
2022-12-13 15:44:25 +0100

    add folder validation and file isEmpty in server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express =require('express')
 const mongoose= require('mongoose')
 const app =express()
+const cors =require("cors")
 const port = 5000
 
 app.use(express.json())
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 const db = "mongodb+srv://Ahmedhenchiri:UvZZCyLXCJU7in18@cluster0.yzf24ly.mongodb.net/trendy-shop?retryWrites=true&w=majority";
 mongoose.set('strictQuery', true)
 mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true })
