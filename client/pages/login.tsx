6de94fb1b25e79e616227eff424151f2d6256629
ahmed henchiri <ahmedhenchiri00@gmail.com>
2022-12-15 22:25:00 +0100

    fix  css

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -5,8 +5,11 @@ import axios from 'axios'
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState<string>("");
-
+  const [error, setError] = useState("");
+ const router = useRouter()
+ console.log('====================================');
+//  console.log(error);
+ console.log('====================================');
    
   async function handleLogin(event: React.FormEvent) {
     event.preventDefault();
@@ -45,6 +48,7 @@ const Login = () => {
               <input
                 type="text"
                 className="form-control"
+                // error={error.email}
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
               />
